0dfd0d1571bbec873a3264e498af18e4aff012e4
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-25 15:59:42 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
-  region     = "us-east-1"
+  region     = "eu-west-1"
   access_key = "AKIA45WHSWTD2VI4DKVW"
   secret_key = "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"
 }
